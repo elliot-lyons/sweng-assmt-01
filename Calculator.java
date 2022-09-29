0f641120b739182ec0b145aa51756823b9dd3d30
elliot-lyons <elliot243lyons@gmail.com>
2022-09-29 14:18:07 +0100

    Added error handling and general flow handling of program.

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,59 @@
+import java.util.*;;
+
 public class Calculator
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello world");
+        boolean quit = false;
+        boolean first = true;
+        
+        Scanner input = new Scanner(System.in);
+
+        while (!quit)
+        {
+            System.out.println((first ? "Welcome to the simple calculator application. " : "") + "Please enter "
+                + "in an expression to start or 'quit' to quit...");
+
+            first = false;
+
+
+                try
+                {
+                    if (input.hasNext("quit"))
+                    {
+                        quit = true;
+                    }
+
+                    else
+                    {
+                        if (validate(input.toString()))
+                        {
+                            int result = calculate(input.toString());
+                            System.out.println("Result = " + result);
+                        }
+
+                        else
+                        {
+                            System.out.println("Invalid input... ");
+                        }
+                    }
+                }
+
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+        }
+
     }
 
     public static boolean validate(String input)
     {
         return false;
+    }
+
+    public static int calculate(String input)
+    {
+        return 0;
     }
 }

157a954a035f1d9acfcbdc87e1d10f4961292930
Hannah Foley <foleyh1@tcd.ie>
2022-10-02 18:01:11 +0100

    completed calculate method and added JUnit tests for it

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,14 @@ import java.util.*;
 
 public class Calculator
 {
+
+    public static Stack<Character> globalOps = new Stack<Character>();
+    public static Stack<Integer> globalNos = new Stack<Integer>();
+
     public static void main(String[] args) 
     {
         boolean quit = false;
+        //this boolean helps with parsing string
         boolean first = true;
         
         Scanner input = new Scanner(System.in);
@@ -15,7 +20,6 @@ public class Calculator
                 + "in an expression to start or 'quit' to quit...");
 
             first = false;
-
 
                 try
                 {
@@ -33,6 +37,17 @@ public class Calculator
                         {                                       // implementation? Certainly make it more efficient
                             int result = calculate(expression);
                             System.out.println("Result = " + result);
+
+                            
+                            while (!globalOps.isEmpty())
+                            {
+                                System.out.println(globalOps.pop());
+                            }
+
+                            while (!globalNos.isEmpty())
+                            {
+                                System.out.println(globalNos.pop());
+                            }
                         }
 
                         else
@@ -51,6 +66,11 @@ public class Calculator
         input.close();
     }
 
+    /*
+        Param: String with expression
+        Returns: Boolean - True if expression is valid, false if not
+
+     */
     public static boolean validate(String input)
     {
         if (input.equals(null))
@@ -59,7 +79,11 @@ public class Calculator
         }
 
         char[] theChars = input.toCharArray();
-        boolean hasToBeNo = true;               // next char has to be number (i.e. at start of sum or after operation)
+        // boolean hasToBeNo = true;               // next char has to be number (i.e. at start of sum or after operation)
+
+        Stack<Character> localOps = new Stack<Character>();
+        Stack<String> localNos = new Stack<String>();
+        int count = 1;
 
         for (int i = 0; i < theChars.length; i++)
         {
@@ -69,12 +93,21 @@ public class Calculator
                 case('*'):
                 case('-'):
                 case('/'):
-                    	if (hasToBeNo || i == (theChars.length - 1))
+                    	if (count == 1 || i == (theChars.length - 1))
                         {
                             return false;
                         }
 
-                        hasToBeNo = true;
+                        localOps.push(theChars[i]);
+                        String current = "";
+
+                        while (count > 1)
+                        {
+                            current = localNos.pop() + current;
+                            count--;
+                        }
+
+                        localNos.push(current);
                         break;
 
                 case('0'):                  // might be a more efficient way of doing this with ASCII?
@@ -87,12 +120,34 @@ public class Calculator
                 case('7'):
                 case('8'):
                 case('9'):
-                        hasToBeNo = false;
+                        count++;
+                        localNos.push(Character.toString(theChars[i]));
                         break;
 
                 default:
                         return false;        // input involves some character that's neither a number nor character
             }
+        }
+        String current = "";
+
+        while (count > 1)
+        {
+            current = localNos.pop() + current;
+            count--;
+        }
+
+        localNos.push(current);
+
+
+        //reverse the stacks by putting them on the global stacks we will use to calculate
+        while (!localOps.isEmpty())
+        {
+            globalOps.push(localOps.pop());
+        }
+
+        while (!localNos.isEmpty())
+        {
+            globalNos.push(Integer.parseInt(localNos.pop()));
         }
 
         return true;
@@ -118,8 +173,29 @@ public class Calculator
         return a / b;
     }
 
+    //@ELLIOT!!!! I think we can remove paramter here since the stacks are global??
     public static int calculate(String input)
     {
-        return 0;
+        while(!globalOps.isEmpty())
+        {
+            int inta = globalNos.pop();
+            int intb = globalNos.pop();
+            switch (globalOps.pop())
+            {
+                case ('+'):
+                    globalNos.push(add(inta, intb));
+                    break;
+                case ('-'):
+                    globalNos.push(subtract(inta, intb));
+                    break;
+                case ('*'):
+                    globalNos.push(multiply(inta, intb));
+                    break;
+                case ('/'):
+                    globalNos.push(divide(inta, intb));
+                    break;
+            }
+        }
+        return globalNos.pop();
     }
 }
